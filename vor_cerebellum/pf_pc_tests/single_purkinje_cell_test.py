181b83a876fe3a092c611db43a09b0a5a19d43d4
Petrut Bogdan <petrut.bogdan@manchester.ac.uk>
2020-12-10 13:29:12 +0000

    PC tests. I think PF-PC looks ok

--- vor_cerebellum/pf_pc_tests/single_purkinje_cell_test.py
+++ vor_cerebellum/pf_pc_tests/single_purkinje_cell_test.py
@@ -1,94 +1,99 @@
-"""
-LTP for PF-PC cells is done via a constant amount for each pre-synaptic spike. This scripts
-tests this behaviour on SpiNNaker.
-"""
+from __future__ import print_function
 import spynnaker8 as p
-import numpy as np
 from pyNN.utility.plotting import Figure, Panel
 import matplotlib.pyplot as plt
 from vor_cerebellum.parameters import (pfpc_min_weight, pfpc_max_weight,
                                        pfpc_ltp_constant, pfpc_t_peak,
                                        rbls, neuron_params)
 
-initial_weight = 0.0
+p.setup(1, min_delay=1, max_delay=15)  # simulation timestep (ms)
+runtime = 500
+# Learning parameters
+min_weight = 0
+max_weight = 0.1
+pot_alpha = 0.01
+t_peak = 100
+initial_weight = 0.05
 plastic_delay = 4
 
-p.setup(1)  # simulation timestep (ms)
 purkinje_cell = p.Population(1,  # number of neurons
                              p.extra_models.IFCondExpCerebellum(**neuron_params),  # Neuron model
-                             label="Purkinje Cell",
                              additional_parameters={"rb_left_shifts": rbls['purkinje']},
+                             label="PC"  # identifier
                              )
 
 # Spike source to send spike via synapse
-spike_times = [101, 201, 301, 401, 501, 601, 701, 801, 901]
+spike_times = [50, 150, 270]
 
 granular_cell = p.Population(1,  # number of sources
                              p.SpikeSourceArray,  # source type
                              {'spike_times': spike_times},  # source spike times
-                             label="src1"  # identifier
+                             label="GrC"  # identifier
                              )
+
+# Spike source to send spike via synapse from climbing fibre
+spike_times_2 = [100, 104, 107, 246]
+climbing_fibre = p.Population(1,  # number of sources
+                              p.SpikeSourceArray,  # source type
+                              {'spike_times': spike_times_2},  # source spike times
+                              label="CF"  # identifier
+                              )
 
 # Create projection from GC to PC
 pfpc_plas = p.STDPMechanism(
-    timing_dependence=p.extra_models.TimingDependencePFPC(t_peak=pfpc_t_peak),
-    weight_dependence=p.extra_models.WeightDependencePFPC(w_min=pfpc_min_weight,
-                                                          w_max=pfpc_max_weight,
-                                                          pot_alpha=pfpc_ltp_constant),
+    timing_dependence=p.extra_models.TimingDependencePFPC(t_peak=t_peak),
+    weight_dependence=p.extra_models.WeightDependencePFPC(w_min=min_weight,
+                                                          w_max=max_weight,
+                                                          pot_alpha=pot_alpha),
     weight=initial_weight, delay=plastic_delay)
 
 synapse_pfpc = p.Projection(
-    granular_cell, purkinje_cell, p.OneToOneConnector(),
+    granular_cell, purkinje_cell, p.AllToAllConnector(),
     synapse_type=pfpc_plas, receptor_type="excitatory")
 
+# Create projection from CF to PC
+synapse = p.Projection(
+    climbing_fibre, purkinje_cell, p.OneToOneConnector(),
+    p.StaticSynapse(weight=0.0, delay=1), receptor_type="excitatory")
+
 granular_cell.record('spikes')
+climbing_fibre.record('spikes')
 purkinje_cell.record("all")
 
-pf_weights = []
-no_runs = len(spike_times)
-run_length = 100
-runtime = run_length * no_runs
-for i in range(no_runs):
-    p.run(run_length)
-    pf_weights.append(synapse_pfpc.get('weight', 'list', with_address=False))
+p.run(runtime)
 
 granluar_cell_spikes = granular_cell.get_data('spikes')
+climbing_fibre_spikes = climbing_fibre.get_data('spikes')
 purkinje_data = purkinje_cell.get_data()
 
-# Release and clean the machine
+pf_weights = synapse_pfpc.get('weight', 'list', with_address=False)
 p.end()
-
-for i in pf_weights:
-    print(i)
-
-pf_weights = np.asarray(pf_weights).ravel()
+print("Final PF-PC weight", pf_weights)
 
 # Plot
-fig = plt.figure(figsize=(4, 4), dpi=400)
-plt.step(np.arange(no_runs) * run_length, pf_weights)
-plt.xlabel("Time (ms)")
-plt.ylabel("PF-PC weights (uS)")
-plt.tight_layout()
-plt.savefig("figures/pc_constant_ltp.png")
-plt.close(fig)
-
 F = Figure(
     # plot data for postsynaptic neuron
     Panel(granluar_cell_spikes.segments[0].spiketrains,
+          ylabel=granular_cell.label,
           yticks=True, markersize=2, xlim=(0, runtime)),
+
+    Panel(climbing_fibre_spikes.segments[0].spiketrains,
+          ylabel=climbing_fibre.label,
+          yticks=True, markersize=2, xlim=(0, runtime)),
+
     Panel(purkinje_data.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
           data_labels=[purkinje_cell.label], yticks=True, xlim=(0, runtime)),
+
     Panel(purkinje_data.segments[0].filter(name='gsyn_exc')[0],
           ylabel="gsyn excitatory (mV)",
           data_labels=[purkinje_cell.label], yticks=True, xlim=(0, runtime)),
+
     Panel(purkinje_data.segments[0].spiketrains,
+          ylabel=purkinje_cell.label,
           yticks=True, markersize=2, xlim=(0, runtime)),
 )
-plt.savefig("figures/pc_single_potentiation.png", dpi=400)
 
-assert np.all(np.isclose(pf_weights,
-                         np.arange(no_runs) * pfpc_ltp_constant,
-                         0.001)), "PF-PC weights are not within 0.001 of the correct value"
+plt.savefig("figures/single_pc_test.png", dpi=400)
 
 print("Done")
