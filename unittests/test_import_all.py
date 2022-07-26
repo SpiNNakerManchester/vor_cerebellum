d3c0947fd60667871cecbfe4b243843e6368f513
Andrew Gait <andrew.gait@manchester.ac.uk>
2022-07-26 11:46:50 +0100

    archived_original_tests deleted, so no need to exclude it any more

--- unittests/test_import_all.py
+++ unittests/test_import_all.py
@@ -22,10 +22,8 @@ class ImportAllModule(unittest.TestCase):
 
     def test_import_all(self):
         if os.environ.get('CONTINUOUS_INTEGRATION', 'false').lower() == 'true':
-            exclusions = ['archived_original_tests']
             package_loader.load_module("vor_cerebellum",
-                                       remove_pyc_files=False,
-                                       exclusions=exclusions)
+                                       remove_pyc_files=False)
         else:
             # Do a full stack cleanup
             package_loader.load_module(
